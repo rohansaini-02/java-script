694a3abd477be3a8fa4daf123b0042be9c27fefd
rohansaini-02 <ranjanasaini700@gmail.com>
2024-08-04 12:31:23 +0000

    Add

--- first.js
+++ first.js
@@ -1,4 +1,12 @@
-// console.log("Rohan Saini 0916");
-// console.log("First Java Script Code");
-fullName = "Rohan Saini";
-console.log(fullName)
+/*SO guys this is your Rohan Saini brand ambassador of polaris school of technology
+console.log("Rohan Saini 0916");
+console.log("First Java Script Code");*/
+// fullName = "Rohan Saini";
+// console.log(fullName);
+let a = 7;
+let b = 3;
+console.log("a=",a ,"& b=",b);
+console.log("a+b=" , a+b);
+console.log("a-b=" , a-b);
+console.log("a*b=" , a*b);
+console.log("a/b=" , a/b);
