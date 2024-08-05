fc9ec60c17470dbc0a9ca31b49d578efa1d17122
rohansaini-02 <ranjanasaini700@gmail.com>
2024-08-05 17:09:07 +0000

    Add

--- first.js
+++ first.js
@@ -3,10 +3,34 @@ console.log("Rohan Saini 0916");
 console.log("First Java Script Code");*/
 // fullName = "Rohan Saini";
 // console.log(fullName);
-let a = 7;
-let b = 3;
-console.log("a=",a ,"& b=",b);
-console.log("a+b=" , a+b);
-console.log("a-b=" , a-b);
-console.log("a*b=" , a*b);
-console.log("a/b=" , a/b);
+// let a = 7;
+// let b = 3;
+// console.log("a=",a ,"& b=",b);
+// console.log("a+b=" , a+b);
+// console.log("a-b=" , a-b);
+// console.log("a*b=" , a*b);
+// console.log("a/b=" , a/b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let a=7;
+let b=3;
+
+console.log("a++=",a)
